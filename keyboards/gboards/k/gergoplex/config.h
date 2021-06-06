e5b1f2bb89837dacedab2922d98816d40e1df466
nbassett <nate@nbassett.com>
2021-06-05 20:12:36 -0700

    Initial, broken commit

--- keyboards/gboards/k/gergoplex/config.h
+++ keyboards/gboards/k/gergoplex/config.h
@@ -45,7 +45,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_MAX_SPEED      7
 #define MOUSEKEY_WHEEL_DELAY 0
 #define TAPPING_TOGGLE  1
-#define TAPPING_TERM    200
+#define TAPPING_TERM    171
 #define IGNORE_MOD_TAP_INTERRUPT 
 #define LOCKING_SUPPORT_ENABLE
 #define LOCKING_RESYNC_ENABLE
